f4cf53a47800d10cb34be9d2a5b2350fb20876bd
DireRaven22075 <59801734+DireRaven22075@users.noreply.github.com>
2022-08-19 19:40:47 +0900

    Update CharacterManager.cs

--- Assets/Scripts/DireRaven22075/CharacterManager.cs
+++ Assets/Scripts/DireRaven22075/CharacterManager.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class CharacterManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CapsuleCollider2D))]
+[RequireComponent(typeof(Animation))]
+[RequireComponent(typeof(AudioSource))]
+public class CharacterManager : MonoBehaviour {
+    void Start() {
         
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    int x = 0, y = 0;
+    void Update() {
+        if (Input.GetKey(KeyCode.A)) { x--; }
+        if (Input.GetKey(KeyCode.D)) { x++; }
     }
 }
