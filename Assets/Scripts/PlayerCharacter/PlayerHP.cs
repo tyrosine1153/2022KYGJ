19a50755cffdd6d4a89c7a1345468d94494e0f24
whdrnr <openinhere1@gmail.com>
2022-08-21 09:01:14 +0900

    1

--- Assets/Scripts/PlayerCharacter/PlayerHP.cs
+++ Assets/Scripts/PlayerCharacter/PlayerHP.cs
@@ -1,18 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHP : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public List<Image> HP_Img = new List<Image>();
 
-    // Update is called once per frame
     void Update()
     {
-        
+        HP_Check();
+    }
+
+    void HP_Check()
+    {
+        var character = FindObjectOfType<CharacterController>().hp;
+        Debug.Log(character);
+
+        switch(character)
+        {
+            case 0:
+                break;
+
+            case 1:
+                HP_Img[0].enabled = true;
+                HP_Img[1].enabled = false;
+                HP_Img[2].enabled = false;
+                HP_Img[3].enabled = false;
+                break;
+
+            case 2:
+                HP_Img[0].enabled = true;
+                HP_Img[1].enabled = true;
+                HP_Img[2].enabled = false;
+                HP_Img[3].enabled = false;
+                break;
+
+            case 3:
+                HP_Img[0].enabled = true;
+                HP_Img[1].enabled = true;
+                HP_Img[2].enabled = true;
+                HP_Img[3].enabled = false;
+                break;
+
+            case 4:
+                HP_Img[0].enabled = true;
+                HP_Img[1].enabled = true;
+                HP_Img[2].enabled = true;
+                HP_Img[3].enabled = true;
+                break;
+
+
+        }
     }
 }
